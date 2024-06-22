d30c531eea2b96192f03a7c2d3608b2d51675f5c
Saicke1 <sarah.manicke@yahoo.de>
2024-06-22 07:51:14 +0200

    removed App component to an extra file App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,29 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import GlobalStyle from './GlobalStyle';
-import store from './store';
-import Container from './components/Container';
-import H4 from './components/H4';
-import Input from './components/Input';
-import Button from './components/Button';
-
-const App = () => {
-  return (
-    <Container>
-      <H4>FACEIT Tournaments</H4>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Input placeholder="Search tournament ..." />
-        <Button>CREATE TOURNAMENT</Button>
-      </div>
-    </Container>
-  );
-};
+import tournamentsStore from './store';
+import { App } from './App';
 
 const container = document.getElementById('root');
 if (!container) {
@@ -32,8 +11,8 @@ if (!container) {
 }
 const root = createRoot(container);
 root.render(
-  <Provider store={store}>
+  <Provider store={tournamentsStore}>
     <GlobalStyle />
-    <App />
+    <App props={tournamentsStore} />
   </Provider>
 );
