1ed5ac661c833b736c0165277625af4824990b9a
Saicke1 <sarah.manicke@yahoo.de>
2024-06-22 07:55:55 +0200

    renamed the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const tournamentsStore = createStore(rootReducer, applyMiddleware(thunk));
 
-export default store;
+export default tournamentsStore;
